4c6c124509b8302591254ee7bed2ebc346994457
24host <90661740+24host@users.noreply.github.com>
2021-10-28 21:50:58 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import tkinter
+import time
+import threading
+
+
+def hi():
+    time.sleep(2)
+    print("hi")
+    
+    
+mainwindow = tkinter.Tk()
+mainwindow.title("Best Discord Token Generator | hCaptcha Bypass *Latest Method")
+mainwindow.geometry("720x720") 
+mainwindow.configure(bg="#141414")
+
+
+label = tkinter.Label(mainwindow, text="Updated: 28/10/2021 | Source for sale", borderwidth=3, relief="solid", padx=5, pady=10).pack(padx=5, pady=10)
+label = tkinter.Label(mainwindow, text="Discord Token Generator", font=("Arial", 33))
+label.pack()
+button = tkinter.Button(mainwindow, text="discord.gg/FQTfmdbWhG", command=threading.Thread(target=hi).start())
+button.pack()
+
+mainwindow.mainloop()
